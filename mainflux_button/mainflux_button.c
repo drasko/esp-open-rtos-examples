8cee3b327b69a1b947011331a1784cc310719c04
Drasko DRASKOVIC <drasko.draskovic@gmail.com>
2016-04-13 01:32:51 +0200

    Connected to Mainflux
    Signed-off-by: Drasko DRASKOVIC <drasko.draskovic@gmail.com>

--- mainflux_button/mainflux_button.c
+++ mainflux_button/mainflux_button.c
@@ -16,6 +16,8 @@
 
 #include <semphr.h>
 
+#include <stdlib.h>
+
 
 /* You can use http://test.mosquitto.org/ to test mqtt_client instead
  * of setting up your own MQTT server */
@@ -26,9 +28,11 @@
 #define MQTT_PASS NULL
 
 
-#define MAINFLUX_DEV_ID "FluxButton"
+#define MAINFLUX_DEV_ID "fb001"
 #define MAINFLUX_MQTT_TOPIC "/1234/"MAINFLUX_DEV_ID"/attributes/message"
 #define MAINFLUX_MQTT_MESSAGE "ORBIT"
+
+static uint8_t on = 0;
 
 xSemaphoreHandle wifi_alive;
 xSemaphoreHandle button_push;
@@ -42,19 +46,19 @@ const int gpio = 0;   /* gpio 0 usually has "PROGRAM" button attached */
 const gpio_inttype_t int_type = GPIO_INTTYPE_EDGE_NEG;
 #define GPIO_HANDLER gpio00_interrupt_handler
 
-static void  topic_received(MessageData *md)
+static void topic_received(MessageData *md)
 {
-	int i;
-	MQTTMessage *message = md->message;
-	printf("Received: ");
-	for( i = 0; i < md->topic->lenstring.len; ++i)
-		printf("%c", md->topic->lenstring.data[ i ]);
+    int i;
+    MQTTMessage *message = md->message;
+    printf("Received: ");
+    for( i = 0; i < md->topic->lenstring.len; ++i)
+        printf("%c", md->topic->lenstring.data[ i ]);
 
-	printf(" = ");
-	for( i = 0; i < (int)message->payloadlen; ++i)
-		printf("%c", ((char *)(message->payload))[i]);
+    printf(" = ");
+    for( i = 0; i < (int)message->payloadlen; ++i)
+        printf("%c", ((char *)(message->payload))[i]);
 
-	printf("\r\n");
+    printf("\r\n");
 }
 
 static const char *  get_my_id(void)
@@ -84,138 +88,145 @@ static const char *  get_my_id(void)
 
 static void  mqttTask(void *pvParameters)
 {
-	int ret			= 0;
-	struct Network network;
-	MQTTClient client	= DefaultClient;
-	char mqtt_client_id[20];
-	uint8_t mqtt_buf[100];
-	uint8_t mqtt_readbuf[100];
-	MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
+    int ret = 0;
+    struct Network network;
+    MQTTClient client = DefaultClient;
+    char mqtt_client_id[20];
+    uint8_t mqtt_buf[100];
+    uint8_t mqtt_readbuf[100];
+    MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
 
-	NewNetwork( &network );
-	memset(mqtt_client_id, 0, sizeof(mqtt_client_id));
-	strcpy(mqtt_client_id, "ESP-");
-	strcat(mqtt_client_id, get_my_id());
+    NewNetwork( &network );
+    memset(mqtt_client_id, 0, sizeof(mqtt_client_id));
+    strcpy(mqtt_client_id, "ESP-");
+    strcat(mqtt_client_id, get_my_id());
 
-	while(1)
+    while(1)
     {
-		xSemaphoreTake(wifi_alive, portMAX_DELAY);
-		printf("%s: started\n", __func__);
-		printf("%s: (Re)connecting to MQTT server %s ... ",__func__,
-			       	MQTT_HOST);
-		ret = ConnectNetwork(&network, MQTT_HOST, MQTT_PORT);
-		if (ret)
+        xSemaphoreTake(wifi_alive, portMAX_DELAY);
+        printf("%s: started\n", __func__);
+        printf("%s: (Re)connecting to MQTT server %s ... ",__func__,
+                    MQTT_HOST);
+        ret = ConnectNetwork(&network, MQTT_HOST, MQTT_PORT);
+        if (ret)
         {
-			printf("error: %d\n", ret);
-			taskYIELD();
-			continue;
-		}
-		printf("done\n");
-		NewMQTTClient(&client, &network, 5000, mqtt_buf, 100, 
-				mqtt_readbuf, 100);
+            printf("error: %d\n", ret);
+            taskYIELD();
+            continue;
+        }
+        printf("done\n");
+        NewMQTTClient(&client, &network, 5000, mqtt_buf, 100, 
+                mqtt_readbuf, 100);
 
-		data.willFlag		= 0;
-		data.MQTTVersion	= 3;
-		data.clientID.cstring	= mqtt_client_id;
-		data.username.cstring	= MQTT_USER;
-		data.password.cstring	= MQTT_PASS;
-		data.keepAliveInterval	= 10;
-		data.cleansession	= 0;
-		
+        data.willFlag = 0;
+        data.MQTTVersion = 3;
+        data.clientID.cstring = mqtt_client_id;
+        data.username.cstring = MQTT_USER;
+        data.password.cstring = MQTT_PASS;
+        data.keepAliveInterval = 10;
+        data.cleansession = 0;
+
         printf("Send MQTT connect ... ");
-		ret = MQTTConnect(&client, &data);
-		if(ret)
+        ret = MQTTConnect(&client, &data);
+        if(ret)
         {
-			printf("error: %d\n", ret);
-			DisconnectNetwork(&network);
-			taskYIELD();
-			continue;
-		}
-		printf("done\n");
-		
+            printf("error: %d\n", ret);
+            DisconnectNetwork(&network);
+            taskYIELD();
+            continue;
+        }
+        printf("done\n");
+
         /** Subscribe to the topic */
         MQTTSubscribe(&client, "/FluxButtonRx", QOS1, topic_received);
 
-		while(1)
+        while(1)
         {
 
-			char msg[PUB_MSG_LEN - 1] = MAINFLUX_MQTT_MESSAGE;
+            //char msg[PUB_MSG_LEN - 1] = MAINFLUX_MQTT_MESSAGE;
+            char msg[PUB_MSG_LEN - 1] = "";
 
-		    xSemaphoreTake(button_push, portMAX_DELAY);
+            if (on)
+                on = 0;
+            else
+                on = 1;
+            itoa(on, msg, 10);
 
-			printf("Got message to publish\n");
-			MQTTMessage message;
-			message.payload	= msg;
-			message.payloadlen = PUB_MSG_LEN;
-			message.dup = 0;
-			message.qos = QOS1;
-			message.retained = 0;
-			ret = MQTTPublish(&client, MAINFLUX_MQTT_TOPIC, &message);
-			if (ret != SUCCESS)
+            xSemaphoreTake(button_push, portMAX_DELAY);
+
+            printf("Got message to publish\n");
+            MQTTMessage message;
+            message.payload = msg;
+            message.payloadlen = PUB_MSG_LEN;
+            message.dup = 0;
+            message.qos = QOS1;
+            message.retained = 0;
+            ret = MQTTPublish(&client, MAINFLUX_MQTT_TOPIC, &message);
+            if (ret != SUCCESS)
             {
-				printf("error while publishing message: %d\n", ret );
-			}
+                printf("error while publishing message: %d\n", ret );
+            }
 
-			ret = MQTTYield(&client, 1000);
-			if (ret == DISCONNECTED)
-				break;
-		}
-		printf("Connection dropped, request restart\n\r");
-		taskYIELD();
-	}
+            ret = MQTTYield(&client, 1000);
+            if (ret == DISCONNECTED)
+                break;
+        }
+        printf("Connection dropped, request restart\n\r");
+        taskYIELD();
+    }
 }
 
 static void  wifiTask(void *pvParameters)
 {
-	uint8_t status	= 0;
-	uint8_t retries = 30;
-	struct sdk_station_config config = {
-		.ssid = WIFI_SSID,
-		.password = WIFI_PASS,
-	};
+    uint8_t status  = 0;
+    uint8_t retries = 30;
+    struct sdk_station_config config = {
+        .ssid = WIFI_SSID,
+        .password = WIFI_PASS,
+    };
 
-	printf("WiFi: connecting to WiFi\n\r");
-	sdk_wifi_set_opmode(STATION_MODE);
-	sdk_wifi_station_set_config(&config);
+    printf("WiFi: connecting to WiFi\n\r");
+    sdk_wifi_set_opmode(STATION_MODE);
+    sdk_wifi_station_set_config(&config);
 
-	while(1)
-	{
-		while ((status != STATION_GOT_IP) && (retries))
+    while(1)
+    {
+        while ((status != STATION_GOT_IP) && (retries))
         {
-			status = sdk_wifi_station_get_connect_status();
-			printf("%s: status = %d\n", __func__, status );
-			if ( status == STATION_WRONG_PASSWORD )
+            status = sdk_wifi_station_get_connect_status();
+            printf("%s: status = %d\n", __func__, status );
+            if ( status == STATION_WRONG_PASSWORD )
             {
-				printf("WiFi: wrong password\n");
-				break;
-			} else if (status == STATION_NO_AP_FOUND)
+                printf("WiFi: wrong password\n");
+                break;
+            } else if (status == STATION_NO_AP_FOUND)
             {
-				printf("WiFi: AP not found\n");
-				break;
-			} else if (status == STATION_CONNECT_FAIL) 
+                printf("WiFi: AP not found\n");
+                break;
+            } else if (status == STATION_CONNECT_FAIL) 
             {
-				printf("WiFi: connection failed\n");
-				break;
-			}
-			vTaskDelay( 1000 / portTICK_RATE_MS );
-			--retries;
-		}
-		if (status == STATION_GOT_IP)
+                printf("WiFi: connection failed\n");
+                break;
+            }
+            vTaskDelay( 1000 / portTICK_RATE_MS );
+            --retries;
+        }
+        if (status == STATION_GOT_IP)
         {
-			printf("WiFi: Connected\n");
-			xSemaphoreGive(wifi_alive);
-			taskYIELD();
-		}
+            printf("WiFi: Connected\n");
+            xSemaphoreGive(wifi_alive);
+            taskYIELD();
+        }
 
-		while ((status = sdk_wifi_station_get_connect_status()) == STATION_GOT_IP)
+        while ((status = sdk_wifi_station_get_connect_status()) == STATION_GOT_IP)
         {
-			xSemaphoreGive(wifi_alive);
-			taskYIELD();
-		}
-		printf("WiFi: disconnected\n");
-		sdk_wifi_station_disconnect();
-		vTaskDelay( 1000 / portTICK_RATE_MS );
-	}
+            xSemaphoreGive(wifi_alive);
+            taskYIELD();
+        }
+        printf("WiFi: disconnected\n");
+        sdk_wifi_station_disconnect();
+        vTaskDelay( 1000 / portTICK_RATE_MS );
+    }
 }
 
 /* This task configures the GPIO interrupt and uses it to tell
@@ -262,8 +273,8 @@ void user_init(void)
     vSemaphoreCreateBinary(button_push);
 
     /** Take both semaphores (to "arm" them), as they are "given" by default */
-	xSemaphoreTake(wifi_alive, portMAX_DELAY);
-	xSemaphoreTake(button_push, portMAX_DELAY);
+    xSemaphoreTake(wifi_alive, portMAX_DELAY);
+    xSemaphoreTake(button_push, portMAX_DELAY);
 
     tsqueue = xQueueCreate(2, sizeof(uint32_t));
 
